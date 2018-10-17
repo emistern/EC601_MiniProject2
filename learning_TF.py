ce32eb4ae9a37b7a4cdd398eea1c135d80d08d2b
Emily Stern <emistern@bu.edu>
2018-10-17 11:49:11 -0400

    Load in Fashion MNIST data

--- learning_TF.py
+++ learning_TF.py
@@ -1,1 +1,21 @@
 #This program will be used to learn TensorFlow, using the examples from https://github.com/tensorflow/workshops, "MNIST"
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+# get the version of TensorFlow
+# print(tf.__version__)
+
+# Import MNIST Dataset
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+# Create a list of all of the labels
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
