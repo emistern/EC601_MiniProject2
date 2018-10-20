276bc4e79fd3b2c9b33d35eb8ed802524c396f39
Emily Stern <emistern@bu.edu>
2018-10-20 14:53:13 -0400

    Added Libaries to use

--- organizing_data.py
+++ organizing_data.py
@@ -1,2 +1,11 @@
 # The purpose of this program is to go into the downloaded cocodataset and get the desired Categories to use
 # for training an image classifier.
+
+# This follows the cocodatasetAPI Python Demo found here: https://github.com/cocodataset/cocoapi/blob/master/PythonAPI/pycocoDemo.ipynb
+
+# Import Needed Libraries
+from pycocotools.coco import COCO
+import numpy as np
+import skimage.io as io
+import matplotlib.pyplot as plt
+import pylab
