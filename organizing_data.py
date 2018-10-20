841c7d3633e77acd93ad52b5f522dc38e8cbcea6
Emily Stern <emistern@bu.edu>
2018-10-20 16:22:21 -0400

    Write the split training/validation/testing datasets to individual files.

--- organizing_data.py
+++ organizing_data.py
@@ -1,6 +1,7 @@
 # The purpose of this program is to go into the downloaded cocodataset and get the desired Categories to use
 # for training an image classifier. It will then shuffle the images and split them by 80/20 training/testing
-# and then 80/20 training/validation. It will return a txt file with the names of the image files
+# and then 80/20 training/validation. It will return three separate txt files with the names of the image files
+# in each dataset
 
 # This follows the cocodatasetAPI Python Demo found here: https://github.com/cocodataset/cocoapi/blob/master/PythonAPI/pycocoDemo.ipynb
 
@@ -53,9 +54,21 @@ print(len(imgIds))
 # What is 80% of the images list?
 eighty_pct_full_images = int(.8*len(imgIds))
 print('80% of images is: ', eighty_pct_full_images)
-training_dataset = imgIds[:eighty_pct_full_images]
+big_training_dataset = imgIds[:eighty_pct_full_images]
 testing_dataset = imgIds[eighty_pct_full_images:]
+eighty_pct_training = int(.8*len(big_training_dataset))
+print('80% of Training images is: ', eighty_pct_training)
+training_dataset = big_training_dataset[:eighty_pct_training]
+validation_dataset = big_training_dataset[eighty_pct_training:]
 
+# Write the training, testing, and validation data sets to a file
+with open("training_dataset.txt", "w") as f:
+    f.writelines(map("{}\n".format, training_dataset))
 
+with open("validation_dataset.txt", "w") as f:
+    f.writelines(map("{}\n".format, validation_dataset))
+
+with open("testing_dataset.txt", "w") as f:
+    f.writelines(map("{}\n".format, testing_dataset))
 
 
