49f8deaf6f2c609eea175869d1d718de00db2b67
Emily Stern <emistern@bu.edu>
2018-10-23 18:02:01 -0400

    Modified categories to use

--- organizing_data.py
+++ organizing_data.py
@@ -35,21 +35,20 @@ nms = set([cat['supercategory'] for cat in cats])
 
 
 # get all images containing given categories, select one at random
-#catIds = coco.getCatIds(catNms=['chair','person']);
+#catIds = coco.getCatIds(catNms=['chair','toilet']);
 catIds_chair = coco.getCatIds(catNms=['chair']);
 imgIds_chair = coco.getImgIds(catIds=catIds_chair);
-catIds_person = coco.getCatIds(catNms=['person']);
-imgIds_person = coco.getImgIds(catIds=catIds_person);
-
-# Get the number of images in dataset
-
+catIds_toilet = coco.getCatIds(catNms=['toilet']);
+imgIds_toilet = coco.getImgIds(catIds=catIds_toilet);
 
 # Make the datasets the same size
-smaller_class = min(len(imgIds_chair), len(imgIds_person))
+smaller_class = min(len(imgIds_chair), len(imgIds_toilet))
 shortened_imgIds_chair = imgIds_chair[:smaller_class]
-shortened_imgIds_person = imgIds_person[:smaller_class]
+shortened_imgIds_toilet = imgIds_toilet[:smaller_class]
 
-
+#################################################################
+######## Split the datasets into training,val, test #############
+#################################################################
 # # Need to split the dataset into training, testing, and validation set. 
 # # split the entire list to have 80/20 training vs testing
 
@@ -63,19 +62,56 @@ print('80% of Training images is: ', eighty_pct_training)
 training_dataset_chair = big_training_dataset_chair[:eighty_pct_training]
 validation_dataset_chair = big_training_dataset_chair[eighty_pct_training:]
 
-# What is 80% of the person list?
-big_training_dataset_person = shortened_imgIds_person[:eighty_pct_full_images]
-testing_dataset_person = shortened_imgIds_person[eighty_pct_full_images:]
-training_dataset_person = big_training_dataset_person[:eighty_pct_training]
-validation_dataset_person = big_training_dataset_person[eighty_pct_training:]
+# What is 80% of the toilet list?
+big_training_dataset_toilet = shortened_imgIds_toilet[:eighty_pct_full_images]
+testing_dataset_toilet = shortened_imgIds_toilet[eighty_pct_full_images:]
+training_dataset_toilet = big_training_dataset_toilet[:eighty_pct_training]
+validation_dataset_toilet = big_training_dataset_toilet[eighty_pct_training:]
 
 
 #################################################################
 ######## Move the training items to correct Folders #############
 #################################################################
-# Copy the chair images into the training folder
+# # Copy the chair images into the training folder
+# image_file_chair = []
+# for image in training_dataset_chair:
+# 	image = str(image)
+# 	paddedimage = image.zfill(12)
+# 	image_name = paddedimage+'.jpg'
+# 	image_file_chair.append(image_name)
+
+# source = "./Coco_Dataset/val2017/"
+# dest = "./training/chair"
+# for image in image_file_chair:
+# 	try:
+# 		shutil.copy2(source+image, dest)
+# 	except:
+# 		pass
+
+# # # Copy the toilet images into the training folder
+# image_file_toilet = []
+# for image in training_dataset_toilet:
+# 	image = str(image)
+# 	paddedimage = image.zfill(12)
+# 	image_name = paddedimage+'.jpg'
+# 	image_file_toilet.append(image_name)
+
+# source = "./Coco_Dataset/val2017/"
+# dest = "./training/toilet"
+# for image in image_file_toilet:
+# 	try:
+# 		shutil.copy2(source+image, dest)
+# 	except:
+# 		pass
+
+
+
+#################################################################
+###### Move the each specified class of photos to it's dir ######
+#################################################################
+# Copy the chair images into the chair folder
 image_file_chair = []
-for image in training_dataset_chair:
+for image in shortened_imgIds_chair:
 	image = str(image)
 	paddedimage = image.zfill(12)
 	image_name = paddedimage+'.jpg'
@@ -89,22 +125,21 @@ for image in image_file_chair:
 	except:
 		pass
 
-# # Copy the person images into the training folder
-image_file_person = []
-for image in training_dataset_person:
+# # Copy the toilet images into the toilet folder
+image_file_toilet = []
+for image in shortened_imgIds_toilet:
 	image = str(image)
 	paddedimage = image.zfill(12)
 	image_name = paddedimage+'.jpg'
-	image_file_person.append(image_name)
+	image_file_toilet.append(image_name)
 
 source = "./Coco_Dataset/val2017/"
-dest = "./training/person"
-for image in image_file_person:
+dest = "./training/toilet"
+for image in image_file_toilet:
 	try:
 		shutil.copy2(source+image, dest)
 	except:
 		pass
-
 
 
 
